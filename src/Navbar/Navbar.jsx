b94cd2ba27f177bbf122ee6a2de719f0a393bad4
Elizabeth Bey <ebey04@gmail.com>
2026-01-29 11:17:26 -0500

    feat(navbar): add logo and style for Link, add media query to hide text on mobile

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';   
 import styles from './Navbar.module.css'
+import sanctuary from '../assets/sanctuary.png'
 
 export default function Navbar() {
     return (
         <nav className={styles.navbar} role="navigation" aria-label="Main navigation">
             <div className={styles.navbarContainer}>
-                <Link to="/" className={styles.navbarBrand}>
-                    Rescue Track App
-                </Link>
+                <div className={styles.logoCenter}>
+                    <Link to="/" className={styles.navbarBrand}>
+                        <img src={sanctuary} alt="Rescue Track App Logo" className={styles.navbarLogo} />
+                        <div className={styles.appTitle}>Rescue Track</div>
+                    </Link>
+                </div>
                 <ul className={styles.navbarMenu}>
                     <li className={styles.navbarItem}>
                         <Link to="/animals" className={styles.navbarLink}>
