330f5106b773b9204ea280dd9b76e40f3f336481
Elizabeth Bey <ebey04@gmail.com>
2026-01-30 17:42:51 -0500

    feat(AnimalsProvider): create provider file and wrap <App/> in main.jsx with it

---
+++ src/AnimalsProvider.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { AnimalsContext } from './AnimalsContext';
+
+export function AnimalsProvider({ children }) {
+    const [animals, setAnimals] = useState([]);
+
+    const addAnimal = (newAnimal) => {
+        setAnimals(prev => [...prev, newAnimal]);
+    };
+
+    const updateAnimal = (animalId, updatedFields) => {
+        setAnimals(prev =>
+        prev.map(animal =>
+            animal.id === animalId
+            ? { ...animal, ...updatedFields }
+            : animal
+        )
+        );
+    };
+
+    const addNote = (animalId, noteText) => {
+        const note = {
+        text: noteText,
+        date: new Date().toLocaleString(),
+        };
+
+        setAnimals(prev =>
+        prev.map(animal =>
+            animal.id === animalId
+            ? {
+                ...animal,
+                notes: animal.notes ? [...animal.notes, note] : [note],
+                }
+            : animal
+        )
+        );
+    };
+
+    return (
+        <AnimalsContext.Provider
+        value={{
+            animals,
+            addAnimal,
+            updateAnimal,
+            addNote,
+        }}
+        >
+        {children}
+        </AnimalsContext.Provider>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { AnimalsProvider } from './AnimalsProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AnimalsProvider>
+        <App />
+      </AnimalsProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
